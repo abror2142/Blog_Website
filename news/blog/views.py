bf901b3d67a0f00485e131a65fafede8e838c260
Abrorbek Ismoilov <abror2142@gmail.com>
2024-04-01 10:57:58 +0500

    CRUD Operator has been implemented and mistakes corrected

--- news/blog/views.py
+++ news/blog/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from .models import Category, Post
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import PostForm
 
 
 def all_posts(request):
@@ -12,3 +13,49 @@ def all_posts(request):
     }
     return render(request, "blog/index.html", context=context)
 
+
+def all_posts_by_category_id(request, category_id):
+    posts = Post.objects.filter(category_id=category_id)
+    categories = Category.objects.all()
+    context = {
+        'posts': posts,
+        'categories': categories
+    }
+    return render(request, "blog/index.html", context=context)
+
+
+def post_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    context = {
+        'post': post
+    }
+    return render(request, 'blog/post_detail.html', context=context)
+
+
+def post_create(request):
+    form = PostForm(data=request.POST)
+    if form.is_valid():
+        form.save()
+        return redirect('index')
+    form = PostForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'blog/post_form.html', context=context)
+
+
+def post_update(request, pk):
+    post = Post.objects.get(pk=pk)
+    form = PostForm(data=request.POST or None, instance=post)
+    if form.is_valid():
+        form.save()
+        return redirect('index')
+    return render(request, 'blog/post_form.html', {'form': form})
+
+
+def post_delete(request, pk):
+    post = Post.objects.get(pk=pk)
+    if request.method == 'POST':
+        post.delete()
+        return redirect('index')
+    return render(request, 'blog/post_delete.html', {"post": post})
